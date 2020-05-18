ec63f65d658641d0905ed7b95cc898b2cfa2c7ae
mikenath223 <michgoldennathan17@gmail.com>
2020-05-19 00:21:54 +0100

    [Feature] Add signin and signup components

---
+++ src/components/singin.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Button from 'react-bootstrap-button-loader';
+import PropTypes from 'prop-types';
+import style from '../styles/reg.module.css';
+
+
+const Singin = ({ handleSignInChange, handleSigninSubmit, load }) => (
+  <div className={`${style.modal} modal signin-modal `}>
+    <span className={`${style.close} close close-sign `}>&times;</span>
+    <form method="post" className={`${style.form} form signin-form `} onSubmit={handleSigninSubmit}>
+      <h1>Sign In</h1>
+      <label htmlFor="username">
+        Username
+        <input type="text" name="username" onChange={handleSignInChange} required />
+      </label>
+
+      <label htmlFor="password">
+        Password
+        <input type="password" name="password" onChange={handleSignInChange} required />
+      </label>
+      <Button loading={load} className={`but ${style.but} ${style.signBut}`}>Sign In</Button>
+    </form>
+  </div>
+);
+
+Singin.propTypes = {
+  handleSignInChange: PropTypes.func.isRequired,
+  handleSigninSubmit: PropTypes.func.isRequired,
+  load: PropTypes.bool.isRequired,
+};
+
+export default Singin;
---
+++ src/components/singup.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Button from 'react-bootstrap-button-loader';
+import style from '../styles/reg.module.css';
+
+
+const Singup = ({ handleRegChange, handleRegSubmit, load }) => (
+  <div className={`${style.modal} modal reg-modal `}>
+    <span className={`${style.close} close close-reg`}>&times;</span>
+    <form method="post" className={`${style.form} form reg-form `} onSubmit={handleRegSubmit}>
+      <h1>Register</h1>
+      <label htmlFor="username">
+        Username
+        {' '}
+        <br />
+        <input type="text" name="username" onChange={handleRegChange} required />
+      </label>
+
+      <label htmlFor="password">
+        Password
+        {' '}
+        <br />
+        <input type="password" name="password" minLength="6" onChange={handleRegChange} required />
+      </label>
+      <label htmlFor="password-confirmation">
+        Confirm Password
+        {' '}
+        <br />
+        <input type="password" name="password-conf" minLength="6" onChange={handleRegChange} required />
+      </label>
+      <Button loading={load} className={`${style.but} but`}>Register</Button>
+    </form>
+  </div>
+);
+
+Singup.propTypes = {
+  handleRegChange: PropTypes.func.isRequired,
+  handleRegSubmit: PropTypes.func.isRequired,
+  load: PropTypes.bool.isRequired,
+};
+
+export default Singup;
