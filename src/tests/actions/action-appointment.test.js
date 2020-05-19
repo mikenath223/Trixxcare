89f0dbab98570e2eabd418de0ca8debc9a89c4e7
mikenath223 <michgoldennathan17@gmail.com>
2020-05-19 04:11:33 +0100

    [Test] Create redux appointment, authentication and docs actions tests

---
+++ src/tests/actions/action-appointment.test.js
@@ -0,0 +1,28 @@
+import { SETAPPOINT, DELAPPOINT } from '../../actions/index';
+
+
+describe('appointment creation', () => {
+  it('should create an action to add an appointment', () => {
+    const appoint = {
+      doc_name: 'John Doe',
+      date: '12/11/2020 01:22',
+    };
+    const expectedAction = {
+      type: 'SET APPOINT',
+      appoint,
+    };
+    expect(SETAPPOINT(appoint)).toEqual(expectedAction);
+  });
+
+  it('should create an action to remove an appointment', () => {
+    const appoint = {
+      doc_name: 'John Doe',
+      date: '12/11/2020 01:22',
+    };
+    const expectedAction = {
+      type: 'DEL APPOINT',
+      appoint,
+    };
+    expect(DELAPPOINT(appoint)).toEqual(expectedAction);
+  });
+});
---
+++ src/tests/actions/action-authentication.test.js
@@ -0,0 +1,50 @@
+import { SETLOGIN, SETLOGOUT, SETSIGNIN } from '../../actions/index';
+
+
+describe('appointment creation', () => {
+  it('should create an action to login a user', () => {
+    const auth = {
+      isLogged: true,
+      user: 'Mike',
+    };
+    const expectedAction = {
+      type: 'SET LOGIN',
+      auth,
+    };
+    expect(SETLOGIN(auth)).toEqual(expectedAction);
+  });
+
+  it('should create an action to logout a user', () => {
+    const auth = {
+      isLogged: false,
+      user: 'Mike',
+    };
+    const expectedAction = {
+      type: 'SET LOGOUT',
+      auth,
+    };
+    expect(SETLOGOUT(auth)).toEqual(expectedAction);
+  });
+
+  it('should create an action to trigger show signin form', () => {
+    const trigger = {
+      show: true,
+    };
+    const expectedAction = {
+      type: 'TRIGGER SIGNIN',
+      trigger,
+    };
+    expect(SETSIGNIN(trigger)).toEqual(expectedAction);
+  });
+
+  it('should create an action to not trigger show signin form', () => {
+    const trigger = {
+      show: false,
+    };
+    const expectedAction = {
+      type: 'TRIGGER SIGNIN',
+      trigger,
+    };
+    expect(SETSIGNIN(trigger)).toEqual(expectedAction);
+  });
+});
