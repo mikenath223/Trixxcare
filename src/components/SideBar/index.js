94c9c057a16c5a55f81404938550df65007a1b58
mikenath223 <michgoldennathan17@gmail.com>
2021-07-16 17:39:16 +0100

    Handle active tab toggle on dashboard

--- src/components/SideBar/index.js
+++ src/components/SideBar/index.js
@@ -1,37 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { handleCloseMenu } from 'utils/domlist';
 import PropTypes from 'prop-types';
 import Logo from 'assets/images/logo.png';
 import style from './SideBar.module.css';
 
-const SideBar = ({ auth, children, logged }) => (
-  <div className={`${style.sideBar} sideBar`}>
-    <button onClick={handleCloseMenu} type="button" onKeyDown={() => { }} className={`${style.closeMenu} btn`}>
-      <img src="https://img.icons8.com/color/30/000000/close-window.png" alt="" />
-    </button>
-    <p className={style.user}>
-      {auth.user && <img src="https://img.icons8.com/windows/35/000000/user-male-circle.png" alt="" />}
-      {' '}
-      {auth.user && auth.user.substring(0, 7)}
-    </p>
-    <Link to="/">
-      <img className={style.logo} src={Logo} alt="" />
-    </Link>
-    <div className={style.midBar}>
-      <p>
-        <Link to="/doctors" tabIndex="-1">CAREGIVER</Link>
+const SideBar = ({ auth, children, logged }) => {
+  const [activeTab, setActiveTab] = useState('caregiver')
+  
+  return (
+    <div className={`${style.sideBar} sideBar`}>
+      <button onClick={handleCloseMenu} type="button" onKeyDown={() => { }} className={`${style.closeMenu} btn`}>
+        <img src="https://img.icons8.com/color/30/000000/close-window.png" alt="" />
+      </button>
+      <p className={style.user}>
+        {auth.user && <img src="https://img.icons8.com/windows/35/000000/user-male-circle.png" alt="" />}
+        {' '}
+        {auth.user && auth.user.substring(0, 7)}
       </p>
-      {logged ? (
-        <p>
-          {' '}
-          <Link to="/appointments" tabIndex="-2">APPOINTMENTS</Link>
+      <Link to="/">
+        <img className={style.logo} src={Logo} alt="" />
+      </Link>
+      <div className={style.midBar}>
+        <p className={activeTab === 'caregiver' && style.activeTab} onClick={() => setActiveTab('caregiver')}>
+          <Link to="/doctors" tabIndex="-1">CAREGIVER</Link>
         </p>
-      ) : null}
+        {logged ? (
+          <p className={activeTab === 'appointments' && style.activeTab} onClick={() => setActiveTab('appointments')}>
+            {' '}
+            <Link to="/appointments" tabIndex="-2">APPOINTMENTS</Link>
+          </p>
+        ) : null}
+      </div>
+      {children}
     </div>
-    {children}
-  </div>
-);
+  )
+};
 
 SideBar.propTypes = {
   auth: PropTypes.shape({
